0882ebe0c10151db38112f44b3418897a7036ae2
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2023-03-02 18:15:09 +0300

    fix: ssr

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,9 @@
 import type { ModuleOptions } from './types';
 
 export const libraryName = 'vue-map-ui';
-
-export const rootComponentName = 'VMap';
-
 export const runtimeDir = './runtime';
 
 const allComponents = ['VMap', 'VMapTileLayer', 'VMapOsmTileLayer'];
-
 const allImports: string[] = ['useMap', 'useLayer'];
 
 export const defaults: ModuleOptions = {
--- src/core/components.ts
+++ src/core/components.ts
@@ -1,21 +1,11 @@
 import { addComponent, type Resolver } from '@nuxt/kit';
-import { libraryName, rootComponentName, runtimeDir } from '../config';
+import { libraryName, runtimeDir } from '../config';
 import { toArray } from '../utils';
 import type { ModuleOptions } from '../types';
 
 export function resolveComponents(config: ModuleOptions, resolver: Resolver) {
   const { components } = config;
   const allComponents = new Set(components);
-
-  addComponent({
-    name: rootComponentName,
-    filePath: resolver.resolve(
-      runtimeDir,
-      'components',
-      'ServerPlaceholder.vue'
-    ),
-    mode: 'server'
-  });
 
   for (const item of allComponents) {
     const [name, alias] = toArray(item);
@@ -25,6 +15,16 @@ export function resolveComponents(config: ModuleOptions, resolver: Resolver) {
       name: alias || name,
       filePath: libraryName,
       mode: 'client'
+    });
+
+    addComponent({
+      name: alias || name,
+      filePath: resolver.resolve(
+        runtimeDir,
+        'components',
+        'ServerPlaceholder.vue'
+      ),
+      mode: 'server'
     });
   }
 }
